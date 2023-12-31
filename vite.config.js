cdb82dc7e8d07b90ca4e7ba917048a7b5f3d0f92
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2023-12-31 01:45:57 -0800

    Vituum with Twig setup

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
+import vituum from 'vituum';
+import twig from '@vituum/vite-plugin-twig';
+
 export default {
+    plugins: [
+        vituum(),
+        twig({
+            root: './src',
+        })
+    ],
     build: {
         minify: false,
     },
